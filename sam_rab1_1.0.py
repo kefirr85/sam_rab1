b478f1a9b8a1c7c677d1d4c7636a7c051825abd5
kefirr85 <166715341+kefirr85@users.noreply.github.com>
2024-05-12 11:47:20 +0300

    Add files via upload

---
+++ sam_rab1_1.0.py
@@ -0,0 +1,18 @@
+def print_params(a=10, b='Kef', c=True):
+    print(a, b, c, )
+
+
+print_params()
+print_params('bread')
+print_params('i am KefiRR:', 39 )
+print_params([1,2,3,4,5], ([45,21,13], 'bdbdbdbdbd', (1+2,3*4,5)), False)
+print_params(a=39)
+print_params(c=[15, 20, 30, 40])
+
+values_list = ['Alex', False, [56, 72, 40]]
+values_dict = {'a': 25, 'b': 'KefiRR', 'c': False}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = ['age', 50]
+print_params(*values_list_2, 42)
